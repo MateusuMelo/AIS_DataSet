6ff700b4a1bddd6b494a1a137595ca0fcdaf225d
MateusuMelo <mateus.c@ges.inatel.br>
2022-11-10 16:57:31 -0300

    Organizando funcoes

--- Main.py
+++ Main.py
@@ -2,41 +2,38 @@ import numpy as np
 import pandas as pd
 from datetime import timedelta , datetime
 from sklearn.neighbors import KNeighborsRegressor
-import sklearn.neighbors as sk
+from sklearn.tree import DecisionTreeRegressor
 import matplotlib.pyplot as plt
 
 # Ais data pode ser encontrado nesse link https://coast.noaa.gov/htdata/CMSP/AISDataHandler/2022/
-def load_data():
+def load_data(path):
     data = pd.DataFrame()
-    data = pd.read_csv("AIS_2020_01_01.csv") #arquivo AIS bruto .csv
-
-
-    #horas = data['BaseDateTime'].str.split('T', expand = True)[1].str.split(':', expand = True) # Separanda y/m/d T h:m:s para so h:m:s
+    data = pd.read_csv(path) #arquivo AIS bruto .csv
     data.drop(['Length','Width','Draft','Cargo','Status','TransceiverClass'], inplace = True, axis =1) #Removendo colunas inuteis
     data.dropna() # removendo valores faltantes '?'
     return data
 
 
-def exportVesselCallSign(): #Função para exportar todos os sinais de barcos do dataset, exporta apenas 1 sinal de cada.
-    data = load_data()
+def exportar_IMOs(path): #Função para exportar todos os sinais de barcos do dataset, exporta apenas 1 sinal de cada.
+    data = load_data(path)
     name_list =[]
-    name_list.append(data['CallSign'][0])
+    name_list.append(data['IMO'][0])
 
     for i in range(data.shape[0]):
-        if(data['CallSign'][i] not in name_list):
-            name_list.append(data['CallSign'][i])
+        if(data['IMO'][i] not in name_list):
+            name_list.append(data['IMO'][i])
 
     df = pd.DataFrame(name_list)
-    return df.to_csv("CallSigns.csv", index= False)
+    return df.to_csv("IMO.csv", index= False)
 
-def exportcords(): # função que le a lista sinais e exporta cada DataFrame dos sinais em um arquivo .csv
+def exportar_cordenadas(): # função que le a lista sinais e exporta cada DataFrame dos sinais em um arquivo .csv
     data = load_data()
-    v_sign =  pd.read_csv("CallSigns.csv")
+    v_sign =  pd.read_csv("IMO.csv")
     v_sign.drop([0], inplace = True, axis = 0)
 
 
     for i in range(1,v_sign.shape[0]):
-        data.loc[data['CallSign'] == v_sign.iloc[i,-1]].to_csv("Vessel/" +v_sign.iloc[i,-1] +".csv", index = False)
+        data.loc[data['IMO'] == v_sign.iloc[i,-1]].to_csv("Vessel/" +v_sign.iloc[i,-1] +".csv", index = False)
 
 def seconds_generator(path): #função que converte coluna 'BaseDateTime em um numero inteiro.
     data = pd.read_csv(path)
@@ -50,7 +47,6 @@ def seconds_generator(path): #função que converte coluna 'BaseDateTime em um n
 
 def hour_generator(path): #função que gera horas faltantes entre os intervalos fornecidos.
     seconds_generator(path)
-
     data = pd.DataFrame()
     data = pd.read_csv(path)
 
@@ -75,7 +71,7 @@ def hour_generator(path): #função que gera horas faltantes entre os intervalos
     data_sort.to_csv("Predict/"+path, index=False)
     seconds_generator("Predict/"+path)
 
-def knnimputer(path): #função que implementa o previsor
+def knnimputer(path, n): #função que implementa o previsor
     data_set = pd.DataFrame()
     data_set = pd.read_csv(path)
     data_pred = pd.read_csv("Predict/"+path)
@@ -85,27 +81,31 @@ def knnimputer(path): #função que implementa o previsor
     data_set.sort_values('BaseDateTime')
 
 
-    Knn_lat = KNeighborsRegressor(n_neighbors=7, weights='distance', metric='euclidean')  # sepa que deu certo
+    Knn_lat = KNeighborsRegressor(n_neighbors=n, weights='distance', metric='euclidean')  # sepa que deu certo
     Knn_lat.fit(data_set[['segundos']], data_set[['LAT']])
 
     Knn_lon = KNeighborsRegressor(n_neighbors=7, weights='distance', metric="euclidean")
     Knn_lon.fit(data_set[['segundos']], data_set[['LON']])
 
-    Knn_heading = KNeighborsRegressor(n_neighbors=7, weights='distance', metric="euclidean")
+    Knn_heading = KNeighborsRegressor(n_neighbors=n, weights='distance', metric="euclidean")
     Knn_heading.fit(data_set[['segundos']], data_set[['Heading']])
 
     data_pred['LAT'] = Knn_lat.predict(data_pred[['segundos']])
+
+
     data_pred['LON'] = Knn_lon.predict(data_pred[['segundos']])
     data_pred['Heading'] = Knn_heading.predict(data_pred[['segundos']])
-    new_path = data_pred['CallSign'][0]
+    new_path = data_pred['IMO'][0]
 
     data_pred.to_csv("Predict/Knn/" + new_path + ".csv", index= False)
 
 def grafico(path): #função gera um grafico de comparação dos valores fornecidos e os valores previstos.
     data_set = pd.read_csv(path)
-    data_pred = pd.read_csv("Predict/Knn/" + data_set['CallSign'][0] + ".csv")
+    data_pred = pd.read_csv("Predict/Knn/" + data_set['IMO'][0] + ".csv")
 
     fig,axs = plt.subplots(2)
-    axs[0].scatter(data_set['segundos'],data_set['LAT'])
+    axs[0].scatter(data_set['segundos'],data_set['LAT'],)
     axs[1].scatter(data_pred['segundos'], data_pred['LAT'])
     plt.show()
+
+
